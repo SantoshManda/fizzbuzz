8a130feac42cba61fee37b411a11d9c7d285bd58
Santosh Kumar Manda <kumar.santosh581@gmail.com>
2017-10-26 17:40:38 +0530

    implemented FizzBizzDAO with FizzBizzDAOImpl class

--- src/main/java/com/santosh/fizzbuzz/DAO/FizzBuzzDAOImpl.java
+++ src/main/java/com/santosh/fizzbuzz/DAO/FizzBuzzDAOImpl.java
@@ -8,7 +8,36 @@ public class FizzBuzzDAOImpl implements FizzBuzzDAO {
 	@Override
 	public HashMap<String, ArrayList<Integer>> calculate(Integer UpperLimit) {
 		// TODO Auto-generated method stub
-		return null;
+		if(UpperLimit.intValue() <= 0 || UpperLimit.intValue() == 1 || UpperLimit.intValue() == 2)
+			return null;
+		HashMap<String,ArrayList<Integer>> result = new HashMap<String,ArrayList<Integer>>();
+		ArrayList<Integer> Fizzlist = new ArrayList<Integer>();
+		ArrayList<Integer> Buzzlist = new ArrayList<Integer>();
+		ArrayList<Integer> FizzBuzzlist = new ArrayList<Integer>();
+
+		for(int i = 3; i <= UpperLimit.intValue(); i++)
+		{
+			
+			if( i%3 == 0)
+			{
+				Fizzlist.add(new Integer(i));
+
+			}
+			if(i%5 == 0)
+			{
+				Buzzlist.add(new Integer(i));
+
+			}
+			if(i%15 == 0)
+			{
+				FizzBuzzlist.add(new Integer(i));
+			}
+		}
+		
+		result.put("Fizz", Fizzlist);
+		result.put("Buzz", Buzzlist);
+		result.put("FizzBuzz" , FizzBuzzlist);
+		return result;
 	}
 
 }
