f935a3b592ce1f2d03c1ec244075e513b2ea1421
Santosh Kumar Manda <kumar.santosh581@gmail.com>
2017-10-26 17:59:09 +0530

    changed FizzBizzDAOImpl class method calculate to use results of modulo by 3 and modulo by 5 to form list for FizzBuzz instead of computing i%15

--- src/main/java/com/santosh/fizzbuzz/DAO/FizzBuzzDAOImpl.java
+++ src/main/java/com/santosh/fizzbuzz/DAO/FizzBuzzDAOImpl.java
@@ -14,21 +14,23 @@ public class FizzBuzzDAOImpl implements FizzBuzzDAO {
 		ArrayList<Integer> Fizzlist = new ArrayList<Integer>();
 		ArrayList<Integer> Buzzlist = new ArrayList<Integer>();
 		ArrayList<Integer> FizzBuzzlist = new ArrayList<Integer>();
-
+		boolean r3=false,r5=false;
 		for(int i = 3; i <= UpperLimit.intValue(); i++)
 		{
+			r3=false;
+			r5=false;
 			
 			if( i%3 == 0)
 			{
 				Fizzlist.add(new Integer(i));
-
+				r3=true;
 			}
 			if(i%5 == 0)
 			{
 				Buzzlist.add(new Integer(i));
-
+				r5=true;
 			}
-			if(i%15 == 0)
+			if(r3 && r5)
 			{
 				FizzBuzzlist.add(new Integer(i));
 			}
