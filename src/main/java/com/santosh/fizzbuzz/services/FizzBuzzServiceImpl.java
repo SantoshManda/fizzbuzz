e5885af44c9af5cef1b86e58aa5efce3d51962f3
Santosh Kumar Manda <kumar.santosh581@gmail.com>
2017-10-26 17:29:13 +0530

    implemented FizzBuzzService with FizzBuzzServiceImpl class

--- src/main/java/com/santosh/fizzbuzz/services/FizzBuzzServiceImpl.java
+++ src/main/java/com/santosh/fizzbuzz/services/FizzBuzzServiceImpl.java
@@ -2,13 +2,17 @@ package com.santosh.fizzbuzz.services;
 
 import java.util.ArrayList;
 import java.util.HashMap;
+import org.springframework.beans.factory.annotation.Autowired;
 
 public class FizzBuzzServiceImpl implements FizzBuzzService {
 
+	@Autowired
+	private FizzBuzzDAO dao;
+	
 	@Override
 	public HashMap<String, ArrayList<Integer>> process(Integer UpperLimit) {
 		// TODO Auto-generated method stub
-		return null;
+		return dao.calculate(UpperLimit);
 	}
 
 }
